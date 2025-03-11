23dbdcecb9a5b52d3c50e241da5d1523fdf084da
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-03-11 08:04:20 -0300

    timer and dots done

--- image-carousel.js
+++ image-carousel.js
@@ -6,6 +6,7 @@ export default class ImageCarousel {
         this.currentImageIndex = currentImageIndex;
         this.width = width;
         this.height = height;
+        this.startTimer();
     }
     
   
@@ -26,19 +27,17 @@ export default class ImageCarousel {
         //for nextImage and previousImage functions
         this.imageElement = currentImage;
         
-        //buttons
+        //buttons positioning
         carouselContainer.style.position = 'relative';
         //next button
         const nextButton = document.createElement('button');
         nextButton.textContent = '>';
         nextButton.style.width = '50px';
         nextButton.style.height = '50px';
-        
         nextButton.style.position = 'absolute';
         nextButton.style.right = '10px';
         nextButton.style.top = '50%';
         nextButton.style.transform = 'translateY(-50%)';
-        
         carouselContainer.appendChild(nextButton);
         
         //left button
@@ -46,24 +45,62 @@ export default class ImageCarousel {
         previousButton.textContent = '<';
         previousButton.style.width = '50px';
         previousButton.style.height = '50px';
-        
         previousButton.style.position = 'absolute';
         previousButton.style.left = '10px';
         previousButton.style.top = '50%';
         previousButton.style.transform = 'translateY(-50%)';
-        
         carouselContainer.appendChild(previousButton);
         
         //button logic
         nextButton.addEventListener('click', () => this.nextImage());
         previousButton.addEventListener('click', () => this.previousImage());
+
+        //navigation dots
+        const dotsContainer = document.createElement('div');
+        dotsContainer.style.position = 'absolute';
+        dotsContainer.style.bottom = '10px';
+        dotsContainer.style.left = '50%';
+        dotsContainer.style.transform = 'translateX(-50%)';
+        for (let i = 0; i < this.arrayOfImagePaths.length; i++) {
+            const navigationDot = document.createElement('span');
+            navigationDot.classList.add('dot');
+            navigationDot.style.width = '10px';
+            navigationDot.style.height = '10px';
+            navigationDot.style.borderRadius = '50%';
+            navigationDot.style.backgroundColor = 'gray';
+            navigationDot.style.display = 'inline-block';
+            navigationDot.style.margin = '0 5px';
+            navigationDot.dataset.index = i;
+            navigationDot.style.border = '1px solid black';
+            if (i === this.currentImageIndex) {
+                navigationDot.style.backgroundColor = 'white';
+            }
+           
+            navigationDot.addEventListener('click', (event) => {
+                const targetIndex = parseInt(event.target.dataset.index);
+                this.changeImage(targetIndex);
+                this.currentImageIndex = targetIndex;
+            });
+            
+                    
+            dotsContainer.appendChild(navigationDot);
+        }
+                
+        this.dotsArray = dotsContainer.querySelectorAll('.dot');
+
         
+        carouselContainer.appendChild(dotsContainer);
+
         return carouselContainer;
     }
 
-    //change img src
-    changeImage() {
-        this.imageElement.src = this.arrayOfImagePaths[this.currentImageIndex];
+    //change img.src and dot color
+    changeImage(targetIndex) {
+        this.imageElement.src = this.arrayOfImagePaths[targetIndex];
+        this.dotsArray.forEach(dot => {
+            dot.style.backgroundColor = 'gray';
+        });
+        this.dotsArray[targetIndex].style.backgroundColor = 'white';
     }
         
 
@@ -74,7 +111,7 @@ export default class ImageCarousel {
             this.currentImageIndex = 0;
         }
         console.log(this.currentImageIndex);
-        this.changeImage()
+        this.changeImage(this.currentImageIndex)
     }
 
     //subtract one from currentImageIndex
@@ -84,8 +121,13 @@ export default class ImageCarousel {
             this.currentImageIndex = this.arrayOfImagePaths.length - 1;
         }
         console.log(this.currentImageIndex);
-        this.changeImage()
+        this.changeImage(this.currentImageIndex)
     }
 
-    
+    //start timer
+    startTimer() {
+        setInterval(() => {
+            this.nextImage();
+        }, 5000);
+    }
 }
